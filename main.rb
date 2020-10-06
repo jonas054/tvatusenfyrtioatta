04e4f6d824e849f068e3964d957084741b2b0f76
Jonas Arvidsson <jonas054@gmail.com>
2020-10-06 20:52:41 +0200

    Single source file

--- main.rb
+++ main.rb
@@ -2,8 +2,139 @@
 
 require 'forwardable'
 require 'gosu'
-require './screen'
-require './board'
+
+# Handles presentation.
+class Screen
+  COLORS = {
+    -1   => "\e[48;5;228m\e[38;5;0m", # black on bright yellow
+    2    => "\e[48;5;88m",            # webmaroon
+    4    => "\e[48;5;90m",            # webpurple
+    8    => "\e[48;5;72m",            # seagreen
+    16   => "\e[48;5;102m",           # dimgray
+    32   => "\e[48;5;55m",            # indigo
+    64   => "\e[48;5;105m",           # mediumslateblue
+    128  => "\e[48;5;106m",           # olivedrab
+    256  => "\e[48;5;109m",           # cadetblue
+    512  => "\e[48;5;202m",           # orangered
+    1024 => "\e[48;5;39m",            # dodgerblue
+    2048 => "\e[48;5;118m"            # lawngreen
+  }.freeze
+
+  def initialize(board)
+    @board = board
+    print "\e[2J" # Clear screen.
+    print "\e[?25l" # Hide cursor.
+  end
+
+  def finish
+    print "\r\e[?25h" # Show cursor.
+  end
+
+  def draw(score)
+    print "\e[0;0H" # Set cursor at top left.
+    puts "#{'+------' * @board.size}+  #{score}"
+    @board.each { |row| draw_row(row) }
+  end
+
+  def draw_row(row)
+    draw_line(row) { '' }
+    draw_line(row) { |cell| cell ? cell.abs.to_s.center(5) : '' }
+    draw_line(row) { '' }
+    puts "\r#{'+------' * @board.size}+"
+  end
+
+  def draw_line(row)
+    print "\r|"
+    row.each { |cell| printf "%s %5s\e[0m|", color(cell), yield(cell) }
+    puts
+  end
+
+  def color(value)
+    value ? COLORS[[value, -1].max] : ''
+  end
+end
+
+# Implements the board with its squares and numbers, but not the representation
+# in terms of colors and lines.
+class Board
+  extend Forwardable
+
+  def_delegators :@squares, :size, :to_a, :each
+
+  def initialize(size)
+    @squares = Array.new(size) { Array.new(size) }
+    [2, 2, 4, 4].each { |value| add_at_random_pos(value) }
+  end
+
+  def make_all_moves(key)
+    before = inspect
+    last = size - 1
+    last.times do
+      Array(0...last).product(Array(0..last)) do |outer_ix, inner_ix|
+        yield move_for_key(outer_ix, inner_ix, key) || 0
+      end
+    end
+
+    clean_up
+    add_at_random_pos(rand < 0.5 ? 2 : 4) if inspect != before
+  end
+
+  def move_for_key(outer_ix, inner_ix, key)
+    last = size - 1
+    # rubocop:disable Layout/ExtraSpacing
+    case key
+    when 'w', "\e[A" then move(inner_ix,        outer_ix,        0 + 1i)
+    when 's', "\e[B" then move(inner_ix,        last - outer_ix, 0 - 1i)
+    when 'a', "\e[D" then move(outer_ix,        inner_ix,        1 + 0i)
+    when 'd', "\e[C" then move(last - outer_ix, inner_ix,       -1 + 0i)
+    end
+    # rubocop:enable Layout/ExtraSpacing
+  end
+
+  # Makes the move and returns the number of points for it.
+  def move(x_pos, y_pos, delta)
+    current = Complex(x_pos, y_pos)
+    other = current + delta
+    if self[current].nil?
+      self[current] = self[other]
+      self[other] = nil
+    elsif self[current] == self[other]
+      self[current] *= -2
+      self[other] = nil
+      -self[current]
+    end
+  end
+
+  def [](complex)
+    @squares[complex.imag][complex.real]
+  end
+
+  def []=(complex, value)
+    @squares[complex.imag][complex.real] = value
+  end
+
+  def add_at_random_pos(value)
+    while true
+      pos = Complex(rand(size), rand(size))
+      break if self[pos].nil?
+    end
+
+    self[pos] = value
+  end
+
+  def clean_up
+    each { |row| row.each_index { |c| row[c] = row[c]&.abs } }
+  end
+
+  def any_possible_moves?(squares = @squares)
+    !squares.flatten.all? ||
+      [squares, squares.transpose].any? { |s| any_adjacent_equal?(s) }
+  end
+
+  def any_adjacent_equal?(squares)
+    squares.any? { |row| row.each_cons(2).any? { |a, b| a == b } }
+  end
+end
 
 Q = Queue.new
 
@@ -44,7 +175,7 @@ class Main
       break unless @board.any_possible_moves?
 
       key = Q.pop
-      break if key == "\u0003" # Ctrl-C
+      break if key == "\C-C"
 
       make_all_moves(key)
     end
@@ -52,11 +183,8 @@ class Main
 
   def make_all_moves(key)
     @board.make_all_moves(key) do |points|
-      if points > 0
-        @score += points
-        @sample.play
-      end
-      @screen.draw(@score)
+      @sample.play if points > 0
+      @screen.draw(@score += points)
       sleep(@sleep_time)
     end
   end
